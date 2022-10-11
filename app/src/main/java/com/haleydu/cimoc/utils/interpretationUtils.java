721033d25d75bf491fa4a46c6eefca091ae63159
Vindroid <vindroid@163.com>
2022-10-11 14:42:06 +0800

    fix(source): chapters turn pages in the wrong order

--- app/src/main/java/com/haleydu/cimoc/utils/interpretationUtils.java
+++ app/src/main/java/com/haleydu/cimoc/utils/interpretationUtils.java
@@ -4,7 +4,6 @@ import com.haleydu.cimoc.model.Comic;
 import com.haleydu.cimoc.source.CCMH;
 import com.haleydu.cimoc.source.Cartoonmad;
 import com.haleydu.cimoc.source.CopyMH;
-import com.haleydu.cimoc.source.HotManga;
 import com.haleydu.cimoc.source.JMTT;
 import com.haleydu.cimoc.source.MH50;
 import com.haleydu.cimoc.source.Manhuatai;
@@ -15,11 +14,11 @@ public class interpretationUtils {
 
     public static boolean isReverseOrder(Comic comic) {
         int type = comic.getSource();
+        // ignore HotManga
         return type == MH50.TYPE ||
                 type == CCMH.TYPE ||
                 type == Cartoonmad.TYPE ||
                 type == CopyMH.TYPE ||
-                type == HotManga.TYPE ||
                 type == MiGu.TYPE ||
                 type == Manhuatai.TYPE ||
                 type == Tencent.TYPE ||
